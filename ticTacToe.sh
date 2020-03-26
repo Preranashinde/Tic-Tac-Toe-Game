eda5d8c12d4a5652eb16f6f61e1aae7cb28eec9a
Preranashinde <61500556+Preranashinde@users.noreply.github.com>
2020-03-26 04:07:38 -0300

    "Check winner or tie"

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -2,73 +2,47 @@
 
 declare -A board
 
-# Variables
-user=0
-computer=1
+player="a"
+turn=0
 noOfRows=3
 noOfColumns=3
-X="x"
-O="o"
-initializeBoard()
-{
-	for (( r=1; r<=$noOfRows; r++ ))
-	do
-		for ((c=1; c<=$noOfColumns; c++ ))
-		do
-			board[r,c]=" "
-		done
-	done
+
+initializeBoard(){
+   for (( r=1; r<=$noOfRows; r++ )); do
+      for ((c=1; c<=$noOfColumns; c++ )); do
+         board[$r,$c]="+"
+      done
+   done
 }
 
-displayBoard()
-{
-	initializeBoard
-
-	for (( r=1; r<=$noOfRows; r++ ))
-	do
-		echo "---------------"
-		for (( c=1; c<=$noOfColumns; c++ ))
-		do
-			printf "| ${board[r,c]} |"
-		done
-		echo
-	done
-	echo "---------------"
-
-}
-letterCheck()
-{
-letterCheck=$((RANDOM%2))
-case $letterCheck in 
-	0) echo "You have assigned : "$O
-	;;
-	1) echo "You have assigned : "$X
-	;;
-esac
+displayBoard(){
+   for (( r=1; r<=$noOfRows; r++ )); do
+      echo "---------------"
+      for (( c=1; c<=$noOfColumns; c++ )); do
+         printf "[ ${board[$r,$c]} ]"
+      done
+      echo
+   done
+   echo "---------------"
 }
 
-tossTheCoin(){
-	if [[ $((RANDOM%2)) -eq 0 ]]; then
-		echo "Player1 Will Play First"
-	else
-		echo "Computer Will Play First"
-	fi
+letterAssign(){
+      playerOne="X"
+      playerTwo="O"
 }
 
-function getTurn()
-{
-	firstTurn=$((RANDOM%2))
-	case $firstTurn in
-		$user)
-			 echo "User plays first"
-		;;
-		$computer)
-			 echo "Computer plays first"
-		;;
-	esac
+toss(){
+   if [[ $((RANDOM%2)) -eq 0 ]]; then
+      echo "Player1 Will Play First"
+   else
+      echo "Player2 Will Play First"
+   fi
 }
-displayBoard
-letterCheck
-tossTheCoin
-getTurn
+
+startToPlay(){
+   letterAssign
+   while [[ $turn -lt 9 && isWon -eq 0 ]]
+   do
+      if [[ $(($turn%2)) -eq 0 ]]; then
+
 
